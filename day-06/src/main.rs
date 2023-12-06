8afa024399197184a5a0cd1431dd4a987f195fab
Alex Hudspith <alexhudspith@users.noreply.github.com>
2023-12-06 20:22:46 +0000

    Day 06: Drop explict a=1 from quadratic

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,9 +1,9 @@
 use std::fmt::Debug;
-use std::str::FromStr;
+use std::num::ParseIntError;
 
 use indoc::indoc;
 use itertools::{Itertools, zip_eq};
-use aoc::aoc_err;
+use aoc::{aoc_err, parse_spaced_vec};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Race {
@@ -11,40 +11,35 @@ struct Race {
     distance: u64
 }
 
-fn quadratic_roots(a: f64, b: f64, c: f64) -> Option<(f64, f64)> {
-    let disc = (b.powi(2) - 4.0 * a * c).sqrt();
-    if disc.is_nan() {
+fn quadratic_roots(b: f64, c: f64) -> Option<(f64, f64)> {
+    let disc_sqrt = (b * b - 4.0 * c).sqrt();
+    if disc_sqrt.is_nan() {
         return None;
     }
 
-    let r1 = (-b + disc) / (2.0 * a);
-    let r2 = (-b - disc) / (2.0 * a);
+    let r1 = (-b - disc_sqrt) / 2.0;
+    let r2 = (-b + disc_sqrt) / 2.0;
     Some((r1, r2))
 }
 
 fn win_count(race: Race) -> u64 {
     let t = race.time as f64;
     let k = race.distance as f64;
-    if let Some((a, b)) = quadratic_roots(1.0, -t, k) {
-        (a.ceil() as u64 - 1) - (b.floor() as u64 + 1) + 1
+    // Solve for integer h, hold time:
+    // distance = h(t - h) > k => h^2 - ht + k < 0
+    if let Some((h1, h2)) = quadratic_roots(-t, k) {
+        (h2.ceil() as u64 - 1) - (h1.floor() as u64 + 1) + 1
     } else {
         // No real roots => distance was impossible => no wins
         0
     }
 }
 
-fn numbers<T: FromStr>(line: &str) -> Result<Vec<T>, T::Err> where T::Err: Debug {
-    line.split_ascii_whitespace().map(|n| n.parse::<T>()).try_collect()
+fn squashed(line: &str) -> Result<u64, ParseIntError> {
+    line.chars().filter(|&c| !c.is_whitespace()).collect::<String>().parse()
 }
 
-fn squashed_number<T: FromStr>(line: &str) -> Result<T, T::Err> where T::Err: Debug {
-    line.chars()
-        .filter(|&c| !c.is_whitespace())
-        .collect::<String>()
-        .parse()
-}
-
-fn parse_races(s: &str, keep_space: bool) -> Result<Vec<Race>, aoc::Error> {
+fn parse_races(s: &str, squash_space: bool) -> Result<Vec<Race>, aoc::Error> {
     let lines = s.split_terminator('\n')
         .filter(|line| !line.is_empty())
         .collect_vec();
@@ -53,12 +48,12 @@ fn parse_races(s: &str, keep_space: bool) -> Result<Vec<Race>, aoc::Error> {
     let times = times.strip_prefix("Time:").ok_or_else(|| aoc_err("No Time row"))?;
     let distances = distances.strip_prefix("Distance:").ok_or_else(|| aoc_err("No Distances row"))?;
 
-    let result = if keep_space {
-        zip_eq(numbers(times)?, numbers(distances)?)
+    let result = if squash_space {
+        vec![Race { time: squashed(times)?, distance: squashed(distances)? }]
+    } else {
+        zip_eq(parse_spaced_vec(times)?, parse_spaced_vec(distances)?)
             .map(|(time, distance)| Race { time, distance })
             .collect_vec()
-    } else {
-        vec![Race { time: squashed_number(times)?, distance: squashed_number(distances)? }]
     };
 
     Ok(result)
@@ -66,13 +61,13 @@ fn parse_races(s: &str, keep_space: bool) -> Result<Vec<Race>, aoc::Error> {
 
 // Answer: 211904
 fn part1(s: &str) -> Result<u64, aoc::Error> {
-    let races = parse_races(s, true)?;
+    let races = parse_races(s, false)?;
     Ok(races.into_iter().map(win_count).product())
 }
 
 // Answer: 43364472
 fn part2(s: &str) -> Result<u64, aoc::Error> {
-    let races = parse_races(s, false)?;
+    let races = parse_races(s, true)?;
     assert_eq!(races.len(), 1);
     Ok(win_count(races[0]))
 }
